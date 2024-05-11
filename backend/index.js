c08198b1cec0b3d9bab8e7c3c1eaf5f372ae4a1a
PiyushW98 <wasekarpiyush98@gmail.com>
2024-05-12 01:58:44 +0530

    connected the database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
-import express from "express"
+import express from "express";
+import { mongoose } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then((result) => {
+    console.log("MongoDB is connected")
+}).catch((err) => {
+    console.error(err);
+});
 
 const app = express();
-const PORT = 5100
 
-app.listen(PORT, () => {
-    console.log(`server has started http://localhost:${PORT}/`)
+app.get('/', (req, res) => { return res.send("Hello World") })
+
+app.listen(process.env.PORT, () => {
+    console.log(`server has started http://localhost:${process.env.PORT}/`)
 })
