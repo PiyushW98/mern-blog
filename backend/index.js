948e609feacfe5a8f00e7545987b13bb97ec1f1f
PiyushW98 <wasekarpiyush98@gmail.com>
2024-05-20 14:47:24 +0530

    signup cpi route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { mongoose } from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./route/user.js"
+import authRouter from "./route/auth.js"
 
 dotenv.config();
 
@@ -12,10 +13,12 @@ mongoose.connect(process.env.MONGO).then((result) => {
 });
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => { return res.send("Hello World") })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`server has started http://localhost:${process.env.PORT}/`)
