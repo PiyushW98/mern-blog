967ad869ae4e910ff72fb5671e9f9c969568d666
PiyushW98 <wasekarpiyush98@gmail.com>
2024-07-29 19:49:47 +0530

    created signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs';
 import errorHandler from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
-const signup = async (req, res, next) => {
+async function signup(req, res, next) {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
@@ -27,4 +28,37 @@ const signup = async (req, res, next) => {
 
 }
 
-export default signup;
+async function signin(req, res, next) {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        return next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            next(errorHandler(400, "Invalid Credentails"));
+        }
+
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+
+        if (!validPassword) {
+            return next(errorHandler(400, "Invalid Credentails"));
+        }
+
+        const token = jwt.sign({
+            id: validUser._id
+        }, process.env.JWT_SECRET);
+
+        const { password: pass, ...rest } = validUser._doc;
+
+        res.status(200).cookie("access token", token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export { signup, signin };
