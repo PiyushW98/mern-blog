b1ecdb1175833643cccabd3e31bedaf444215287
PiyushW98 <wasekarpiyush98@gmail.com>
2024-08-01 22:42:21 +0530

    update user API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs';
-import errorHandler from '../utils/error.js';
+import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 async function signup(req, res, next) {
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO).then(() => { console.log("MongoDB is connect
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('server is running at http:localhost:3000/')
